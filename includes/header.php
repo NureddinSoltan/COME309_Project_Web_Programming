a1751393c96ade9a29e30bddae15ddba0a42be66
NureddinSoltan <noorsoltan40@gmail.com>
2024-12-28 23:22:30 +0300

    add header.php

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="dashboard.php">Dashboard</a> |
+    <a href="books.php">Books</a> |
+    <a href="user/my_books.php">My Books</a> |
+    <a href="logout.php">Logout</a>
+</nav>
+<hr>
