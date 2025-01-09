40c92305bce9cb58f0aceff6a46cac52376aa591
NureddinSoltan <noorsoltan40@gmail.com>
2025-01-09 15:55:50 +0300

    Update the author in books table to NOT NULL

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
 CREATE TABLE books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    author VARCHAR(100),
+    author VARCHAR(100) NOT NULL,
     description TEXT,
     book_file VARCHAR(255),
     book_image VARCHAR(255),
